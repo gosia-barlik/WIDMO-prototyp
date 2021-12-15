92fd18af39476e54615d7ebfd044b2af84c135fd
Malgorzata <malgorzata.barlik@gmail.com>
2021-12-15 18:47:07 +0100

    added steppers

--- src/components/Creator/Creator.jsx
+++ src/components/Creator/Creator.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import StepperHorizontal from "./common/Stepper-horizontal.jsx";
+import StepperVertical from "./common/Stepper-vertical.jsx";
+import'./Creator.css';
 
-export default function Creator() { 
-    return (
-        <div>Działam</div>
-    ) 
+export default function Creator() {
+  return (
+    <>
+    <StepperHorizontal/>
+    <StepperVertical/>
+    </>
+  );
 }
---
+++ src/components/Creator/common/Stepper-horizontal.jsx
@@ -0,0 +1,101 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { styled } from '@mui/material/styles';
+import Stack from '@mui/material/Stack';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import Check from '@material-ui/icons/Check';
+import StepConnector, { stepConnectorClasses } from '@mui/material/StepConnector';
+import './Stepper.css'
+
+
+const QontoConnector = styled(StepConnector)(({ theme }) => ({
+  [`&.${stepConnectorClasses.alternativeLabel}`]: {
+    top: 10,
+    left: 'calc(-50% + 16px)',
+    right: 'calc(50% + 16px)',
+  },
+  [`&.${stepConnectorClasses.active}`]: {
+    [`& .${stepConnectorClasses.line}`]: {
+      borderColor: '#784af4',
+    },
+  },
+  [`&.${stepConnectorClasses.completed}`]: {
+    [`& .${stepConnectorClasses.line}`]: {
+      borderColor: '#784af4',
+    },
+  },
+  [`& .${stepConnectorClasses.line}`]: {
+    borderColor: theme.palette.mode === 'dark' ? theme.palette.grey[800] : '#eaeaf0',
+    borderTopWidth: 3,
+    borderRadius: 1,
+  },
+}));
+
+const QontoStepIconRoot = styled('div')(({ theme, ownerState }) => ({
+  color: theme.palette.mode === 'dark' ? theme.palette.grey[700] : '#eaeaf0',
+  display: 'flex',
+  height: 22,
+  alignItems: 'center',
+  ...(ownerState.active && {
+    color: '#784af4',
+  }),
+  '& .QontoStepIcon-completedIcon': {
+    color: '#784af4',
+    zIndex: 1,
+    fontSize: 18,
+  },
+  '& .QontoStepIcon-circle': {
+    width: 8,
+    height: 8,
+    borderRadius: '50%',
+    backgroundColor: 'currentColor',
+  },
+}));
+
+function QontoStepIcon(props) {
+  const { active, completed, className } = props;
+
+  return (
+    <QontoStepIconRoot ownerState={{ active }} className={className}>
+      {completed ? (
+        <Check className="QontoStepIcon-completedIcon" />
+      ) : (
+        <div className="QontoStepIcon-circle" />
+      )}
+    </QontoStepIconRoot>
+  );
+}
+
+QontoStepIcon.propTypes = {
+  /**
+   * Whether this step is active.
+   * @default false
+   */
+  active: PropTypes.bool,
+  className: PropTypes.string,
+  /**
+   * Mark the step as completed. Is passed to child components.
+   * @default false
+   */
+  completed: PropTypes.bool,
+};
+
+
+const steps = ['Główne informacje', 'Treść ogłoszenia', 'Przetwarzanie danych', 'Podsumowanie'];
+
+export default function StepperHorizontal() {
+  return (
+    <Stack sx={{ width: '80%' }} spacing={4} className="stepper-horizontal-container">
+      <Stepper alternativeLabel activeStep={0} connector={<QontoConnector />}>
+        {steps.map((label) => (
+          <Step key={label}>
+            <StepLabel StepIconComponent={QontoStepIcon}>{label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+
+    </Stack>
+  );
+}
