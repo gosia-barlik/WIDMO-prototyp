2d9abb2dcc2769ff2381eb4254bd1e8e293a4420
Malgorzata <malgorzata.barlik@gmail.com>
2022-02-14 19:25:59 +0100

    Rankomat Step 3 Filters fixes

--- src/components/Rankomat/Step3-Match/Step3-Filters/Step3-Filters.jsx
+++ src/components/Rankomat/Step3-Match/Step3-Filters/Step3-Filters.jsx
@@ -1,35 +1,80 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
 import CustomizedInput from "./Customized-Input";
+import { FormControlLabel, FormGroup, Checkbox } from "@material-ui/core";
+import Button from "@mui/material/Button";
 
 export default function Step3Filters(props) {
-  const qualificationLabel = "Posiada kwalifikację";
-  const qualificationName = [
-    "j.angielski",
-    "zarządzanie cyberbezpieczeństwem",
-    "zarządzanie zespołem",
-    "zapobiega zagrożeniom",
+  const [checked, setChecked] = React.useState([]);
+  let checkboxes = [
+    {
+      name: "Professional skill",
+      value: "professional skill",
+      id: 0,
+      label: "Umiejętności twarde",
+    },
+    {
+      name: "Soft skill",
+      value: "soft skill",
+      id: 1,
+      label: "Umiejętności miękkie",
+    },
+    {
+      name: "Language skill",
+      value: "language skill",
+      id: 2,
+      label: "Umiejętności językowe",
+    },
+    {
+      name: "Education",
+      value: "education",
+      id: 3,
+      label: "Edukacja",
+    },
+    {
+      name: "Experience",
+      value: "experience",
+      id: 4,
+      label: "Doświadczenie",
+    },
+    {
+      name: "Certificates",
+      value: "certificates",
+      id: 5,
+      label: "Certyfikaty",
+    },
+    {
+      name: "Job-position",
+      value: "job-position",
+      id: 6,
+      label: "Nazwa stanowiska",
+    },
+    {
+      name: "Company-name",
+      value: "company-name",
+      id: 7,
+      label: "Nazwa firmy",
+    },
   ];
-
-  const experienceLabel = "Doświadczenie";
-  const experienceLevel = [
-    "mniej niż rok",
-    "przynajmniej 1 rok",
-    "przynajmniej 2 lata",
-    "przynajmniej 3 lata",
-    "przynajmniej 5 lat",
-    "przynajmniej 7 lat",
-    "10 lat i więcej",
-  ];
-
-  const positionLabel = "Stanowisko";
-  const position = ["specjalista", "audytor", "analityk"];
-
-  const companyLabel = "Firma";
-  const companyName = ["Samsung", "Allegro"];
 
   const keyWordLabel = "Słowo kluczowe";
   const keyWord = ["IPS", "technologie DLP", "PKI", "AZURE/MS365"];
+
+  const handleToggle = (checkbox) => () => {
+    const currentIndex = checked.indexOf(checkbox.id);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(checkbox.id);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+    console.log(newChecked);
+    console.log(checkbox.value);
+
+  };
 
   return (
     <>
@@ -37,7 +82,27 @@ export default function Step3Filters(props) {
         Filtry zaawansowane
       </Typography>
 
-      <CustomizedInput
+      <FormGroup row className='o-form checkbox-container'>
+        {checkboxes.map((checkbox) => (
+          <FormControlLabel
+          className={`filters-checkbox-${checkbox.id}`}
+            control={
+              <Checkbox
+                name={checkbox.name}
+                value={checkbox.value}
+                id={checkbox.id}
+                key={checkbox.value}
+                color='primary'
+                onChange={handleToggle(checkbox)}
+                checked={checked.indexOf(checkbox.id) !== -1}
+              />
+            }
+            label={checkbox.label}
+          />
+        ))}
+      </FormGroup>
+
+      {/*<CustomizedInput
         label={qualificationLabel}
         options={qualificationName}
         onSetQualificationName={props.onSetQualificationName}
@@ -56,12 +121,15 @@ export default function Step3Filters(props) {
         label={companyLabel}
         options={companyName}
         onSetCompanyName={props.onSetCompanyName}
-      />
-          <CustomizedInput
+      /> */}
+      <CustomizedInput
         label={keyWordLabel}
         options={keyWord}
         onSetKeyWord={props.onSetKeyWord}
       />
+      <Button variant='contained' className='button-contained' style={{marginLeft:"40%", marginTop:"20px"}}>
+        Filtruj
+      </Button>
     </>
   );
 }
