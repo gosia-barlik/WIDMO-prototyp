0d2e97e049dee3cb95833a90798b22748c4dbe56
Malgorzata <malgorzata.barlik@gmail.com>
2022-10-13 20:05:44 +0200

    landing page texts and styling

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -11,7 +11,9 @@ import { NavLink } from "react-router-dom";
 import AppsIcon from "@material-ui/icons/Apps";
 import Typography from "@mui/material/Typography";
 import background from "../assets/light-purple-background.jpg";
-import creatorIcon from "../assets/creator-screen-shot.jpg";
+import creatorIcon from "../assets/creator-screen-shot.png";
+import analizerIcon from "../assets/analize-cv-screen-shot.png";
+import searchInfographic from "../assets/search-infographic.jpg";
 
 class Landing extends React.Component {
   constructor(props) {
@@ -27,41 +29,6 @@ class Landing extends React.Component {
           className='section-background'
           alt='light purple background'
         />
-        <Typography
-          variant='body'
-          component='div'
-          gutterBottom
-          className='landing-text hero'>
-          <b>AIRA</b> to bezpłatne narzędzie wspomagające proces rekrutacji,
-          wykorzystujące informacje zawarte w <br></br>
-          <b>Zintegrowanym Rejestrze Kwalifikacji </b> oraz aktualnych rynkowych
-          ofertach pracy
-          {/* <span>
-            Wykorzystaj informacje zawarte w{" "}
-            <b>Zintegrowanym Rejestrze Kwalifikacji</b>, aby usprawnić proces
-            rekrutacyjny w swojej firmie.{" "}
-          </span>
-          <span>
-            Zdefiniuj potrzeby rekrutacyjne i stwórz ogłoszenie o pracę w
-            oparciu o szczegółowe informacje zawarte w opisach kwalifikacji w
-            rejestrze.{" "}
-          </span>
-          Zasoby informacji dostępne w ZRK mogą ułatwiać rozwiązywanie problemów
-          związanych z poszukiwaniem i zatrudnianiem nowych pracowników oraz w
-          działaniach na rzecz rozwoju zawodowego kadr. */}
-        </Typography>
-        <Typography
-          variant='body'
-          component='div'
-          gutterBottom
-          className='landing-text primary'
-          style={{ marginTop: "250px" }}></Typography>
-        <Typography
-          variant='body'
-          component='div'
-          gutterBottom
-          className='landing-text primary'
-          style={{ marginTop: "300px" }}></Typography>
 
         <Stack
           spacing={4}
@@ -78,15 +45,65 @@ class Landing extends React.Component {
           }}>
           <Card sx={{ minWidth: 275 }} className='card-container'>
             <Box sx={{ display: "flex", flexDirection: "column" }}>
+              <CardContent
+                className='card1 card-hero'
+                sx={{ flex: "1 0 auto" }}>
+                <div className='hero-text2'>
+                  <Typography
+                    variant='body'
+                    component='div'
+                    className='primary'>
+                    Rekrutuj szybko i skutecznie
+                  </Typography>
+                  <div className='hero-text1'>
+                    <Typography variant='body' component='div'>
+                      AIRA to bezpłatne narzędzie wspomagające proces
+                      rekrutacji, wykorzystujące informacje zawarte w
+                      Zintegrowanym Rejestrze Kwalifikacji oraz aktualnych
+                      rynkowych ofertach pracy
+                    </Typography>
+                  </div>
+                </div>
+              </CardContent>
+              <CardActions className='card1'>
+                <NavLink to='#'>
+                  <Button
+                    variant='contained'
+                    fullWidth
+                    className='button-primary-contained'>
+                    Dowiedz się więcej
+                  </Button>
+                </NavLink>
+              </CardActions>
+            </Box>
+
+            <img
+              src={searchInfographic}
+              className='card-image'
+              alt='job posting creator screen shot'
+            />
+          </Card>
+          <Typography
+            variant='h5'
+            component='div'
+            className='section-title'
+            style={{ height: "40px", marginTop: "80px" }}>
+            Moduły
+          </Typography>
+          <Card className='card-container'>
+            <CardMedia
+              component='img'
+              image={creatorIcon}
+              alt='job posting creator screen shot'
+              sx={{ width: 400 }}
+            />
+            <Box sx={{ display: "flex", flexDirection: "column", width: 400 }}>
               <CardContent className='card1' sx={{ flex: "1 0 auto" }}>
                 <div className='rankomat-text2'>
                   <Typography variant='body' component='div'>
                     Tworzenie ogłoszenia
                   </Typography>
                 </div>
-                {/* <div className='icon-wrapper'>
-                  <AppsIcon />
-                </div> */}
 
                 <div className='rankomat-text1'>
                   <span className='rankomat-text1-0'>
@@ -97,7 +114,7 @@ class Landing extends React.Component {
                     Kreatorze ogłoszeń
                     <br />
                   </span>
-                  <span className='rankomat-text1-36'>
+                  <span className=''>
                     będziesz mógł skorzystać z podpowiedzi generowanych przez
                     algorytmy sztucznej inteligencji. Korzystanie z podpowiedzi
                     jest opcjonalne. Wygenerowane wskazówki czerpią z zasobów
@@ -110,101 +127,60 @@ class Landing extends React.Component {
                 <NavLink to='/creator'>
                   <Button
                     variant='contained'
-                    fullWidth
                     className='button-primary-contained'>
                     Stwórz ogłoszenie
                   </Button>
                 </NavLink>
               </CardActions>
             </Box>
-            <img
-              src={creatorIcon}
-              className='card-image'
-              alt='job posting creator screen shot'
-            />
           </Card>
-
-          <Card sx={{ minWidth: 275 }} className='card-container'>
-            <CardContent className='card1'>
-              <div className='card-color'></div>
-
-              <div className='rankomat-text2'>
-                <Typography variant='body' component='div'>
-                  Dopasuj kandydatów
-                </Typography>
-              </div>
-              <div className='icon-wrapper'>
-                <AppsIcon />
-              </div>
-              <div className='rankomat-text1'>
-                <span className='rankomat-text1-0'>
-                  Korzystając z<br />
-                </span>
-                <span className='rankomat-text1-14'>
-                  Rankomatu życiorysów
-                  <br />
-                </span>
-                <span className='rankomat-text1-36'>
-                  Lorem ipsum dolor sit amet, <br />
-                  consectetur adipiscing elit. <br />
-                  Donec pretium lectus sem, faucibus <br />
-                  consequat lacus pharetra non. <br />{" "}
-                </span>
-              </div>
-            </CardContent>
-            <CardActions className='card1'>
-              <NavLink to='/rankomat'>
-                <Button
-                  variant='contained'
-                  fullWidth
-                  className='button-primary-contained'>
-                  Dopasuj życiorysy
-                </Button>
-              </NavLink>
-            </CardActions>
-          </Card>
-
           <Card
-            sx={{ minWidth: 275 }}
-            className='card-container inspirator-card-container'>
-            <CardContent className='card1'>
-              <div className='card-color'></div>
+            className='card-container'
+            sx={{ display: "flex", flexDirection: "row" }}>
+            <Box sx={{ display: "flex", flexDirection: "column", width: 400 }}>
+              <CardContent className='card1' sx={{ flex: "1 0 auto" }}>
+                <div className='rankomat-text2'>
+                  <Typography variant='body' component='div'>
+                    Analizowanie CV
+                  </Typography>
+                </div>
 
-              <div className='rankomat-text2'>
-                <Typography variant='body' component='div'>
-                  Przygotuj się do rozmowy
-                </Typography>
-              </div>
-              <div className='icon-wrapper'>
-                <AppsIcon />
-              </div>
-
-              <div className='rankomat-text1'>
-                <span className='rankomat-text1-0'>
-                  Dzięki
-                  <br />
-                </span>
-                <span className='rankomat-text1-14'>
-                  {" "}
-                  Inspiratorowi rozmowy
-                  <br />
-                </span>
-                <span className='rankomat-text1-36'>
-                  Lorem ipsum dolor sit amet, <br />
-                  consectetur adipiscing elit. <br />
-                  Donec pretium lectus sem, faucibus <br />
-                  consequat lacus pharetra non. <br />{" "}
-                </span>
-              </div>
-            </CardContent>
-            <CardActions className='card1'>
-              <Button
-                variant='contained'
-                fullWidth
-                className='button-primary-contained'>
-                Przygotuj się do rozmowy
-              </Button>
-            </CardActions>
+                <div className='rankomat-text1'>
+                  <span className='rankomat-text1-0'>
+                    Dzięki modułowi
+                    <br />
+                  </span>
+                  <span className='rankomat-text1-14'>
+                    Analizowania CV
+                    <br />
+                  </span>
+                  <span className='rankomat-text1-36'>
+                    będziesz mógł zaoszczędzić czas na ewaluacji nadesłanych
+                    dokumentów aplikacyjnych. Nasze algorytmy przeanalizują ich
+                    treść pod kątem zawartych w nich informacji na temat
+                    kwalifikacji i umiejętności. Korzystając z filtrów
+                    zaawansowanych sprawnie wyszukasz kluczowe dla siebie
+                    informacje.
+                  </span>
+                </div>
+              </CardContent>
+              <CardActions className='card1'>
+                <NavLink to='/rankomat'>
+                  <Button
+                    variant='contained'
+                    fullWidth
+                    className='button-primary-contained'>
+                    Analizuj CV
+                  </Button>
+                </NavLink>
+              </CardActions>
+            </Box>
+            <CardMedia
+              component='img'
+              image={analizerIcon}
+              alt='cv analizer screen shot'
+              sx={{ width: 400 }}
+            />
           </Card>
         </Stack>
       </Grid>
